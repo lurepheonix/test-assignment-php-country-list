40fce8f1745e8a46a90ccb4bd7ba460f08460fd3
The Ghost <tghostlycat@gmail.com>
2023-04-17 06:36:05 -0400

    fix: set Content-Type header correctly

--- backend/utils/Response.php
+++ backend/utils/Response.php
@@ -7,7 +7,7 @@ class Response
     public static function sendJson($status, $data)
     {
         $json = json_encode($data);
-        header("Content-Type", "application/json");
+        header("Content-Type: application/json");
         header(sprintf('HTTP/1.1 %s %s', 200, $status), true, 200);
 
         echo ($json);
